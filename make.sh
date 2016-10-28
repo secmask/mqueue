09522df9c4b75879548a128a6232e3d3c4493b88
Giang Tran <secmask@gmail.com>
2016-10-28 15:17:56 +0700

    make.sh output file with version

--- make.sh
+++ make.sh
@@ -4,4 +4,4 @@ GOOS=linux
 GOARCH=amd64
 CGO_ENABLED=0
 export GOOS GOARCH CGO_ENABLED
-go build -ldflags "-X main.version=$VERSION`date -u +.%Y%m%d.%H%M%S`.`git rev-parse --short HEAD`" github.com/secmask/mqueue/cmd/mqueue
+go build -o mqueue-$VERSION -ldflags "-X main.version=$VERSION`date -u +.%Y%m%d.%H%M%S`.`git rev-parse --short HEAD`" github.com/secmask/mqueue/cmd/mqueue
